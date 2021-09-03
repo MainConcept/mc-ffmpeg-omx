c80cb1e98a23d37491e6986840cbf497730274b1
bartsevich <bartsevich@scnsoft.com>
2021-09-03 16:15:12 +0300

    OMX plugin: initialize and destroy pthread objects

--- libavcodec/omx_common.c
+++ libavcodec/omx_common.c
@@ -360,6 +360,15 @@ av_cold int omx_cmpnt_init(OMXComponentContext *s)
 
     av_log(s->avctx, AV_LOG_TRACE, "OMX component init\n");
 
+    pthread_mutex_init(&s->state_mutex, NULL);
+    pthread_cond_init(&s->state_cond, NULL);
+    for (int i = 0; i < MAX_PORT_NUMBER; ++i) {
+        pthread_mutex_init(&s->buffers_mutex[i], NULL);
+    }
+    pthread_mutex_init(&s->buffers_cond_mutex, NULL);
+    pthread_cond_init(&s->buffers_cond, NULL);
+    pthread_mutex_init(&s->deiniting_mutex, NULL);
+
     pthread_mutex_lock(&s->deiniting_mutex);
     s->deiniting = OMX_FALSE;
     pthread_mutex_unlock(&s->deiniting_mutex);
@@ -442,6 +451,15 @@ av_cold int omx_cmpnt_end(OMXComponentContext *s)
         s->core.OMX_FreeHandle(s->component);
 
     s->core.OMX_Deinit();
+
+    pthread_mutex_destroy(&s->deiniting_mutex);
+    pthread_cond_destroy(&s->buffers_cond);
+    pthread_mutex_destroy(&s->buffers_cond_mutex);
+    for (int i = 0; i < MAX_PORT_NUMBER; ++i) {
+        pthread_mutex_destroy(&s->buffers_mutex[i]);
+    }
+    pthread_cond_destroy(&s->buffers_cond);
+    pthread_mutex_destroy(&s->state_mutex);
 
     return ret;
 }
